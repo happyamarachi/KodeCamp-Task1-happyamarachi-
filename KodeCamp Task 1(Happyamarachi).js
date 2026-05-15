dfba2765c23d87aa2fb42e3b80f9985a0c7e002c
happyamarachi <happyamarachiu@gmail.com>
2026-05-16 00:27:02 +0100

    KodeCamp Task 1 (Happyamarachi)

---
+++ KodeCamp Task 1(Happyamarachi).js
@@ -0,0 +1,70 @@
+// Q 1. Javascript statement that reverses a string supplied to it
+
+ const greeting = "Hello"
+
+ let str = "Hello"; let reversed = str.split("").reverse("").join("");
+
+console.log(reversed); // "olleH";
+
+
+
+  // Q 3. An if statements that checks a variable and prints out remarks
+
+
+
+ const score = 100;
+
+  if(score >= 70 & score <= 100) {
+ console.log("Excellent");
+ }
+
+
+const score =100
+
+   if(score >= 60 & score <= 69);
+   console.log("Very good");
+        
+
+  const score = 100;
+
+ if(score >= 50  & score <= 59);
+  console.log("Good");
+
+
+  const score = 100;
+ 
+if(score >= 40 & score <= 49);
+console.log("Poor");
+
+    
+  const score = 100;
+
+ if(score >= 0 & score <= 39);
+ console.log("Very poor");
+
+
+const score = 100
+
+if(score >= 0 & score <= 100);
+console.log("Invalide score");
+
+
+
+Q 2. SHORT NOTES ON DATA TYPES AND 3 METHODS EACH
+ A. STRINGS DATA TYPE
+ Strings are text in javascript.they are immutable and written in quotes "", '', or backticks ```.
+  3 COMMON METHODS;
+ 1: toUpperCase()- converts string to uppercase. "hi". toUpperCase() = "Hi"
+ 2: slice(start, end) - extracts part of the string. "Hello" .slice(0, 2) = "He"
+ 3: includes(substring) - checks if substring exists. "Hello" .("lo") = true
+
+ B. NUMBER DATA TYPE
+ Numbers represent both integers and decimals in javascript. There's no seperate float type.
+  3 COMMON METHODS
+  1: toFixed(n) - rounds to n decimal places. 3.14159.toFixed(2) = "3.14"
+  2: parselnt(sting) - converts string to integer.parselnt("42") = 42
+  3: isNAN(value) - checks if value is Not a Number. isNaN("abc") = true
+
+
+
+  
