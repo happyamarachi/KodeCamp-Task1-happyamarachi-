b7170dcc70053da3bea7d55df433f9432de1301f
happyamarachi <happyamarachiu@gmail.com>
2026-05-21 22:03:51 +0100

    KodeCamp Task 1(Happyamarachi)

--- KodeCamp Task 1(Happyamarachi).js
+++ KodeCamp Task 1(Happyamarachi).js
@@ -50,20 +50,20 @@ console.log("Invalide score");
 
 
 
-Q 2. SHORT NOTES ON DATA TYPES AND 3 METHODS EACH
- A. STRINGS DATA TYPE
- Strings are text in javascript.they are immutable and written in quotes "", '', or backticks ```.
-  3 COMMON METHODS;
- 1: toUpperCase()- converts string to uppercase. "hi". toUpperCase() = "Hi"
- 2: slice(start, end) - extracts part of the string. "Hello" .slice(0, 2) = "He"
- 3: includes(substring) - checks if substring exists. "Hello" .("lo") = true
+// Q 2. SHORT NOTES ON DATA TYPES AND 3 METHODS EACH
+//  A. STRINGS DATA TYPE
+//  Strings are text in javascript.they are immutable and written in quotes "", '', or backticks ```.
+//   3 COMMON METHODS;
+//  1: toUpperCase()- converts string to uppercase. "hi". toUpperCase() = "Hi"
+//  2: slice(start, end) - extracts part of the string. "Hello" .slice(0, 2) = "He"
+//  3: includes(substring) - checks if substring exists. "Hello" .("lo") = true
 
- B. NUMBER DATA TYPE
- Numbers represent both integers and decimals in javascript. There's no seperate float type.
-  3 COMMON METHODS
-  1: toFixed(n) - rounds to n decimal places. 3.14159.toFixed(2) = "3.14"
-  2: parselnt(sting) - converts string to integer.parselnt("42") = 42
-  3: isNAN(value) - checks if value is Not a Number. isNaN("abc") = true
+// //  B. NUMBER DATA TYPE
+// //  Numbers represent both integers and decimals in javascript. There's no seperate float type.
+// //   3 COMMON METHODS
+// //   1: toFixed(n) - rounds to n decimal places. 3.14159.toFixed(2) = "3.14"
+// //   2: parselnt(sting) - converts string to integer.parselnt("42") = 42
+// //   3: isNAN(value) - checks if value is Not a Number. isNaN("abc") = true
 
 
 
